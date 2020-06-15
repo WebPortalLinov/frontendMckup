92053ba28d61a4cdf8eeee73e7bc62cbef6f421f
fajar001 <codingpena@gmail.com>
2020-06-15 17:58:00 +0700

    perbaikan fitur multiselect dan penambagan create job alert

--- src/app/modules/job-vacancy/job-vacancy.module.ts
+++ src/app/modules/job-vacancy/job-vacancy.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { JobVacancyListComponent } from './job-vacancy-list/job-vacancy-list.component';
 import { JobVacancyDetailComponent } from './job-vacancy-detail/job-vacancy-detail.component';
 import { JobVacancyComponent } from './job-vacancy/job-vacancy.component';
-import { MultiSelectModule } from 'primeng/primeng';
+import { MultiSelectModule } from 'primeng/multiselect';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { PaginatorModule } from 'primeng/paginator';
 import { DialogModule, CardModule, PanelModule, CarouselModule } from 'primeng/primeng';
+import { DropdownModule } from 'primeng/dropdown';
+import { ChipsModule } from 'primeng/chips';
 
 @NgModule({
   declarations: [JobVacancyListComponent, JobVacancyDetailComponent, JobVacancyComponent],
@@ -20,7 +22,9 @@ import { DialogModule, CardModule, PanelModule, CarouselModule } from 'primeng/p
     DialogModule,
     CardModule,
     PanelModule,
-    CarouselModule
+    CarouselModule,
+    DropdownModule,
+    ChipsModule
   ]
 })
 export class JobVacancyModule { }
