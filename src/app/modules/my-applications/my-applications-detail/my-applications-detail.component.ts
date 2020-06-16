78d7356c6e5fe819c9b6d51872c97e0f14d8fac1
ayusamosir <ayusridevisamosir@gmail.com>
2020-06-16 13:41:21 +0700

    perbaikan size

--- src/app/modules/my-applications/my-applications-detail/my-applications-detail.component.ts
+++ src/app/modules/my-applications/my-applications-detail/my-applications-detail.component.ts
@@ -9,33 +9,32 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 export class MyApplicationsDetailComponent implements OnInit {
   cars: any[];
 
-  responsiveOptions;
+    responsiveOptions;
 
-  constructor() {
+    constructor() { 
+        this.responsiveOptions = [
+            {
+                breakpoint: '1024px',
+                numVisible: 3,
+                numScroll: 3
+            },
+            {
+                breakpoint: '768px',
+                numVisible: 2,
+                numScroll: 2
+            },
+            {
+                breakpoint: '560px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ];
+    }
 
-    this.responsiveOptions = [
-      {
-          breakpoint: '1024px',
-          numVisible: 3,
-          numScroll: 3
-      },
-      {
-          breakpoint: '768px',
-          numVisible: 2,
-          numScroll: 2
-      },
-      {
-          breakpoint: '560px',
-          numVisible: 1,
-          numScroll: 1
-      }
-  ];
-   }
-
-  ngOnInit(){ 
-  //   this.carService.getCarsSmall().then(cars => {
-  //     this.cars = cars
-  // });
-  }
+    ngOnInit() {
+        // this.carService.getCarsSmall().then(cars => {
+        //     this.cars = cars
+        // });
+    }
 
 }
